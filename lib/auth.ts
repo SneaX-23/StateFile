0aaf9e914c77f0b8b8eb787a33c9042198093d7b
SneaX-23 <117501658+SneaX-23@users.noreply.github.com>
2026-08-11 09:54:16 +0530

    add jwt plugin

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,16 +1,20 @@
 import { betterAuth } from "better-auth";
+import { jwt } from "better-auth/plugins";
 import { Pool } from "pg";
 
 export const auth = betterAuth({
   database: new Pool({
-    connectionString: process.env.DB_STRING!,
+    connectionString: process.env.DATABASE_URL,
   }),
   emailAndPassword: {
     enabled: true,
   },
-  socialProviders: {
-
-  },
-  plugins: [jwt()],
-  baseURL: process.env.BETTER_AUTH_URL || "http://localhost:3000",
-})
+  plugins: [
+    jwt({
+      jwt: {
+        expirationTime: "15m",
+      },
+      secret: process.env.BETTER_AUTH_SECRET,
+    }),
+  ],
+});

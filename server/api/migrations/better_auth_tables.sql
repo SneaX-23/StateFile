b249eb63b43c16313b9293a80b61090afc5e2008
SneaX-23 <117501658+SneaX-23@users.noreply.github.com>
2026-08-31 08:40:45 +0530

    add user_profiles and repositories schema. the former stores allowed nos of repos per user and the latter stores the user imported repos info

--- server/api/migrations/better_auth_tables.sql
+++ server/api/migrations/better_auth_tables.sql
@@ -42,8 +42,50 @@ create table "verification" ("id" text not null primary key,
   "updatedAt" timestamptz default CURRENT_TIMESTAMP not null
 );
 
+-- *
+CREATE TABLE "user_profiles" (
+    "id" UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    "userId" TEXT NOT NULL UNIQUE,
+    "tier" VARCHAR(20) NOT NULL DEFAULT 'free',
+    "allowedRepos" INT NOT NULL DEFAULT 3,
+    "createdAt" TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT fk_user_profiles_user
+        FOREIGN KEY ("userId")
+        REFERENCES "user"("id")
+        ON DELETE CASCADE
+);
+
+
+CREATE TABLE "repositories" (
+    "id" UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    "userId" TEXT NOT NULL,
+    "githubRepoId" BIGINT NOT NULL,
+    "repoName" VARCHAR(255) NOT NULL,
+    "defaultBranch" VARCHAR(100) DEFAULT 'main',
+    "isActive" BOOLEAN DEFAULT TRUE,
+    "createdAt" TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "fk_repositories_user"
+        FOREIGN KEY ("userId")
+        REFERENCES "user" ("id")
+        ON DELETE CASCADE,
+
+    CONSTRAINT "unique_user_repo" UNIQUE ("userId", "githubRepoId")
+);
+-- *
+
 create index "session_userId_idx" on "session" ("userId");
 
 create index "account_userId_idx" on "account" ("userId");
 
 create index "verification_identifier_idx" on "verification" ("identifier");
+
+-- *
+-- Index to optimize quota checks during API calls
+CREATE INDEX "idx_user_profiles_user_id" ON "user_profiles"("userId");
+
+CREATE INDEX "idx_repositories_user_id" ON "repositories" ("userId");
+-- *

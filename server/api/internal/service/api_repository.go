a592053491a45cba515f9494a0c3a42364a20706
SneaX-23 <117501658+SneaX-23@users.noreply.github.com>
2026-09-04 23:35:10 +0530

    add new queries methods to ApiRepository

--- server/api/internal/service/api_repository.go
+++ server/api/internal/service/api_repository.go
@@ -8,4 +8,8 @@ import (
 
 type ApiRepository interface {
 	GetAccessToken(ctx context.Context, userid string) (repository.GetAccessTokenRow, error)
+	CheckRepoLimit(ctx context.Context, userid string) (int32, error)
+	AddRepos(ctx context.Context, arg []repository.AddReposParams) (int64, error)
+	DecrementRepoLimit(ctx context.Context, arg repository.DecrementRepoLimitParams) (int32, error)
+	ExecTx(ctx context.Context, fn func(*repository.Queries) error) error
 }

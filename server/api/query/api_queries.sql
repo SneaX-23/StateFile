615f3649cd9ca697bd46a7454478c0ce9d3330d6
SneaX-23 <117501658+SneaX-23@users.noreply.github.com>
2026-09-04 23:23:50 +0530

    add query to check repo limits

--- server/api/query/api_queries.sql
+++ server/api/query/api_queries.sql
@@ -1,2 +1,6 @@
 -- name: GetAccessToken :one
 SELECT "accessToken", "providerId" FROM account WHERE "userId" = $1;
+
+-- name: CheckRepoLimit :one
+SELECT "allowedRepos" from user_profiles WHERE "userId" = $1;
+

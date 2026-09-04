e31520543af9b9a0a53a834aab8ee7c7698e2df1
SneaX-23 <117501658+SneaX-23@users.noreply.github.com>
2026-09-04 23:27:43 +0530

    add queries to decrement user repo limit and to add immported repos

--- server/api/query/api_queries.sql
+++ server/api/query/api_queries.sql
@@ -4,3 +4,17 @@ SELECT "accessToken", "providerId" FROM account WHERE "userId" = $1;
 -- name: CheckRepoLimit :one
 SELECT "allowedRepos" from user_profiles WHERE "userId" = $1;
 
+
+-- name: DecrementRepoLimit :one
+UPDATE user_profiles
+SET "allowedRepos" = "allowedRepos" - $1
+WHERE "userId" = $2 AND "allowedRepos" >= $1
+RETURNING "allowedRepos";
+
+-- name: AddRepos :copyfrom
+INSERT INTO repositories (
+    "userId",
+    "githubRepoId",
+    "repoName"
+) VALUES ($1, $2, $3);
+

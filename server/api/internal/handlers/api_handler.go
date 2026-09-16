fa48c7abca913076b382be09f0fbdde5190b6b24
SneaX-23 <117501658+SneaX-23@users.noreply.github.com>
2026-09-16 21:50:16 +0530

    return repolimt with projects

--- server/api/internal/handlers/api_handler.go
+++ server/api/internal/handlers/api_handler.go
@@ -80,10 +80,10 @@ func (h *Handler) GetProjects(c *gin.Context) {
 		c.JSON(http.StatusInternalServerError, gin.H{"error": "User ID not found in context"})
 		return
 	}
-	projects, err := h.apiService.GetProjectsService(c.Request.Context(), userId.(string))
+	projects, repoLimit, err := h.apiService.GetProjectsService(c.Request.Context(), userId.(string))
 	if err != nil {
 		c.AbortWithStatusJSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
 		return
 	}
-	c.JSON(http.StatusOK, gin.H{"projects": projects})
+	c.JSON(http.StatusOK, gin.H{"projects": projects, "limit": repoLimit})
 }
